e7fe7e94c15a384cc793d72a0d40417893a7ce67
sonota88 <yosiot8753@gmail.com>
2017-01-22 16:07:36 +0900

    get /api/items でダミーデータ返却

--- server.rb
+++ server.rb
@@ -1,6 +1,25 @@
+require "json"
 require "sinatra"
 require "sinatra/reloader"
 
+def _api
+  content_type :json
+  result = yield
+  JSON.generate(result)
+end
+
 get "/hello" do
   "hello #{Time.now}"
+end
+
+get "/api/items" do
+  _api do
+    items = [
+      { id: 1, name: "foo", note: "note 1"},
+      { id: 2, name: "bar", note: "note 2"}
+    ]
+    {
+      items: items
+    }
+  end
 end
--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,18 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-
 var $ = require("jquery");
+
+function puts(... args){
+  console.log.apply(console, args);
+}
+
+function api(method, url, params, fn){
+  var _params = {
+    _method: method,
+    json: JSON.stringify(params)
+  };
+  $.post(url, _params).then(fn);
+}
 
 var Hi = React.createClass({
   render: function() {
@@ -11,6 +22,18 @@ var Hi = React.createClass({
   }
 });
 
-$(()=>{
+function index(){
+  api("get", "/api/items", {}, (data)=>{
+    puts(data);
+  });
   ReactDOM.render(<Hi />, document.getElementById('app'));
+}
+
+$(()=>{
+  console.log(location.href);
+  if( /\/index.html$/.test(location.href) ){
+    index();
+  }else{
+    throw new Error("Invalid URL");
+  }
 });
